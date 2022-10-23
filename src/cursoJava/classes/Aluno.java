4b3927f5ebdd2f837e99f5006da4fc30788aa6f3
Evandro <evandrosilva558@hotmail.com>
2022-10-23 19:50:48 -0300

    creado projeto POO

---
+++ src/cursoJava/classes/Aluno.java
@@ -0,0 +1,16 @@
+package cursoJava.classes;
+
+public class Aluno {
+	
+	String nome;
+	int idade;
+	String dataNascimento;
+	String registroGeral;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculado;
+
+}
---
+++ src/cursoJava/executavel/executaPrograma.java
@@ -0,0 +1,10 @@
+package cursoJava.executavel;
+
+public class executaPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
