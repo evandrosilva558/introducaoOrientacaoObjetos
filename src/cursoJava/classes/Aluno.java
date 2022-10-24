c97975ca9bb978aac112c42d09a949c3fdedc1e4
Evandro <evandrosilva558@hotmail.com>
2022-10-24 18:51:31 -0300

    Metodos acessores do objeto GTTERS e SETTERS.

--- src/cursoJava/classes/Aluno.java
+++ src/cursoJava/classes/Aluno.java
@@ -2,21 +2,116 @@ package cursoJava.classes;
 
 public class Aluno {
 	
-	String nome;
-	int idade;
-	String dataNascimento;
-	String registroGeral;
-	String numeroCpf;
-	String nomeMae;
-	String nomePai;
-	String dataMatricula;
-	String nomeEscola;
-	String serieMatriculado;
+	private String nome;
+	private int idade;
+	private String dataNascimento;
+	private String registroGeral;
+	private String numeroCpf;
+	private String nomeMae;
+	private String nomePai;
+	private String dataMatricula;
+	private String nomeEscola;
+	private String serieMatriculado;
 	
-	public Aluno(String nome, int idade) {
+	public Aluno(String nome, int idade, String dataNascimento,
+			String registroGeral, String numeroCpf,
+			String nomeMae, String nomePai,
+			String dataMatricula, String nomeEscola,
+			String serieMatriculado) {
 		super();
+		
+			this.nome = nome;
+			this.idade = idade;
+			this.dataNascimento = dataNascimento;
+			this.registroGeral = registroGeral;
+			this.numeroCpf = numeroCpf;
+			this.nomeMae = nomeMae;
+			this.nomePai = nomePai;
+			this.dataMatricula = dataMatricula;
+			this.nomeEscola = nomeEscola;
+			this.serieMatriculado = serieMatriculado;
+	}
+	
+	/* Metodos SETTERS e GETTERS */
+	
+	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setIdade(int idade) {
 		this.idade = idade;
+	}
+	
+	public int getIdade() {
+		return idade;
+	}
+	
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+	
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+	
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+	
+	public void setNumeroCpf(String numeroCpf) {
+		this.numeroCpf = numeroCpf;
+	}
+	
+	public String getnumeroCpf() {
+		return numeroCpf;
+	}
+	
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+	
+	public String getNomeMae() {
+		return nomeMae;
+	}
+	
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+	
+	public String getNomePai() {
+		return nomePai;
+	}
+	
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+	
+	public String getdataMatricula() {
+		return dataMatricula;
+	}
+	
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+	
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+	
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
+	
+	public String getSerieMatriculado() {
+		return serieMatriculado;
 	}
 
 }
--- src/cursoJava/executavel/executaPrograma.java
+++ src/cursoJava/executavel/executaPrograma.java
@@ -6,10 +6,13 @@ public class executaPrograma {
 
 	public static void main(String[] args) {
 	
-		Aluno aluno1 = new Aluno("Igor", 31);
-		Aluno aluno2 = new Aluno("Maria", 20);
-		Aluno aluno3 = new Aluno("José", 50);
-
+		Aluno aluno1 = new Aluno();
+		aluno1.setNome("Alex");
+		aluno1.setIdade(32);
+		aluno1.setDataNascimento("18/10/1987");
+		aluno1.setRegistroGeral("414.567.229.0");
+		aluno1.setNumeroCpf("414.338.229.80");
+		
 	}
 
 }
