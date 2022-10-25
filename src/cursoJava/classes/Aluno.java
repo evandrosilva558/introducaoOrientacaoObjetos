35a25a6400fd0921aa1a29f4bcdb60384a104f48
Evandro <evandrosilva558@hotmail.com>
2022-10-25 20:16:53 -0300

    criação das notas do aluno

--- src/cursoJava/classes/Aluno.java
+++ src/cursoJava/classes/Aluno.java
@@ -12,6 +12,11 @@ public class Aluno {
 	private String dataMatricula;
 	private String nomeEscola;
 	private String serieMatriculado;
+	private double nota1;
+	private double nota2;
+	private double nota3;
+	private double nota4;
+	private double media;
 	
 	public Aluno(String nome, int idade, String dataNascimento,
 			String registroGeral, String numeroCpf,
@@ -113,5 +118,44 @@ public class Aluno {
 	public String getSerieMatriculado() {
 		return serieMatriculado;
 	}
+	
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+	
+	public double getNota1() {
+		return nota1;
+	}
+	
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
+	
+	public double getNota2() {
+		return nota2;
+	}
+	
+	public void setNota3(double nota3) {
+		this.nota3 = nota3;
+	}
 
+	public double getNota3() {
+		return nota3;
+	}
+	
+	public void setNota4(double nota4) {
+		this.nota4 = nota4;
+	}
+	
+	public double getNota4() {
+		return nota4;
+	}
+	
+	public void setMedia(double media) {
+		this.media = media;
+	}
+	
+	public double getMedia() {
+		return media;
+	}
 }
